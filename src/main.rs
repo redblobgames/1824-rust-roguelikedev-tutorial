05af06d03527fa64b01fb00bf4a3e421f3a17ef2
Amit Patel <redblobgames@gmail.com>
2018-06-21 18:14:04 -0700

    Initial commit, parts 0, 1, 2
    I did things slightly differently:
    
    * I used the OpenGL backend because the default didn't show up on Mac
    * Instead of erasing each character, I erase the whole screen and
      redraw everything
    * My handle_keys takes the key instead of getting it from tcod

---
+++ src/main.rs
@@ -0,0 +1,138 @@
+extern crate tcod;
+
+use tcod::input::Key;
+use tcod::console::*;
+use tcod::Color;
+use tcod::colors;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const LIMIT_FPS: i32 = 20;
+
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile { blocked: false, block_sight: false }
+    }
+
+    pub fn wall() -> Self {
+        Tile { blocked: true, block_sight: true }
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
+
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+    map
+}
+
+
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color,) -> Self {
+        Object {
+            x: x,
+            y: y,
+            char: char,
+            color: color,
+        }
+    }
+
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    root.clear();
+    
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].block_sight;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+    
+    for object in objects {
+        object.draw(con);
+    }
+
+    blit(&mut console, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
+}
+
+
+fn handle_keys(root: &mut Root, player: &mut Object, key: Key) -> bool {
+    use tcod::input::KeyCode::*;
+    match key {
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
+        Key { code: Escape, .. } => return true,
+        Key { code: Enter, alt: true, .. } => {
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        },
+        _ => {},
+    }
+    false
+}
+
+#[allow(unused)]
+fn main() {
+    let mut root = Root::initializer()
+        .font("../arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .renderer(Renderer::GLSL)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+    let mut console = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
+    let mut objects = [player, npc];
+    
+    while !root.window_closed() {
+        console.clear();
+        
+        root.flush();
+        
+        let key = root.wait_for_keypress(true);
+        if handle_keys(&mut root, &mut objects[0], key) {
+            break;
+        }
+    }
+}
